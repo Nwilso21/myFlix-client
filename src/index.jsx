b7ff409d3c56f79d29c52b2fc32d822b9569ae38
Nwilso21 <noahwilson6381@gmail.com>
2023-11-29 22:23:18 -0700

    Task3 installation
    Created all relevant files and added them to the repository.

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react';
+
+// Import statement to indicate that you need to bundle `./index.scss`
+import "./index.scss";
+
+// Main component (will eventually use all the others)
+const MyFlixApplication = () => {
+  return (
+    <div className="my-flix">
+      <div>Good morning</div>
+    </div>
+  );
+};
