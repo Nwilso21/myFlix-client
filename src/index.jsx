72684d1ecb21f26ab6c87631dc8424a46c3e84b6
Nwilso21 <noahwilson6381@gmail.com>
2023-12-04 17:47:50 -0700

    Component additions
    Created components so that program now displays a list of books and has the functionality to display book details when interacting with a book.

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,12 @@
-import { createRoot } from 'react';
+import { createRoot } from "react-dom/client";
+import { MainView } from "./components/MainView/main-view";
 
-// Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
-// Main component (will eventually use all the others)
-const MyFlixApplication = () => {
-  return (
-    <div className="my-flix">
-      <div>Good morning</div>
-    </div>
-  );
+const App = () => {
+ return <MainView />;
 };
+
+const container = document.querySelector("#root");
+const root = createRoot(container);
+root.render(<App />);
